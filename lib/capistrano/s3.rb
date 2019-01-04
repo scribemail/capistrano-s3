a903e928b609a09098abeaf61beecb268825f2b0
Balazs Kovacs <balazs.kovacs@supercharge.io>
2019-01-04 11:16:58 +0100

    Add option to prefer CloudFront compression supported MIME types

--- lib/capistrano/s3.rb
+++ lib/capistrano/s3.rb
@@ -2,6 +2,7 @@ require "capistrano"
 require "capistrano/s3/publisher"
 require "capistrano/s3/version"
 require "capistrano/s3/defaults"
+require "capistrano/s3/mime_types"
 
 if Gem::Specification.find_by_name('capistrano').version >= Gem::Version.new('3.0.0')
   load File.expand_path('../tasks/capistrano_3.rb', __FILE__)
